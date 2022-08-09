8c0f46a3fa6406972f3d438c28049b7245bed9bb
Kc Udonsi <sr@wsl-ubuntu20.04>
2022-08-08 22:38:26 -0400

    Add appropriate Solidity compilers

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      { version: "0.8.9" },
+      { version: "0.8.7" },
+      { version: "0.7.6" },
+      { version: "0.6.6" },
+      { version: "0.6.0" },
+      { version: "0.5.0" }
+    ]
+  },
 };
