1af3e64d6c28e58f209a53e5aca802a8529c260c
Kc Udonsi <sr@wsl-ubuntu20.04>
2022-08-14 02:53:15 -0400

    Lesson 3 chapter 7 starter code

--- contracts/lesson3/zombiefeeding.sol
+++ contracts/lesson3/zombiefeeding.sol
@@ -38,15 +38,18 @@ contract ZombieFeeding is ZombieFactory {
     return (_zombie.readyTime <= now);
   }
 
+  // 1. Make this function internal
   function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
+    // 2. Add a check for `_isReady` here
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
     if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
       newDna = newDna - newDna % 100 + 99;
     }
     _createZombie("NoName", newDna);
+    // 3. Call `_triggerCooldown`
   }
 
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
